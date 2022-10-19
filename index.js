ead550dbef8ba6187a94bae16c4f406eaf0041e2
Soumydeep2308 <87338582+Soumydeep2308@users.noreply.github.com>
2022-10-19 12:52:08 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    $(".h1").mouseenter(function(){
+        $(".P1").fadeIn(400).delay(600).removeClass("none");
+        $(".P1").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h1").mouseleave(function(){
+        $(".P1").fadeOut(400).delay(600).removeClass("block");
+        $(".P1").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h2").mouseenter(function(){
+        $(".P2").fadeIn(400).delay(600).removeClass("none");
+        $(".P2").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h2").mouseleave(function(){
+        $(".P2").fadeOut(400).delay(600).removeClass("block");
+        $(".P2").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h3").mouseenter(function(){
+        $(".P3").fadeIn(400).delay(600).removeClass("none");
+        $(".P3").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h3").mouseleave(function(){
+        $(".P3").fadeOut(400).delay(600).removeClass("block");
+        $(".P3").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h4").mouseenter(function(){
+        $(".P4").fadeIn(400).delay(600).removeClass("none");
+        $(".P4").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h4").mouseleave(function(){
+        $(".P4").fadeOut(400).delay(600).removeClass("block");
+        $(".P4").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h5").mouseenter(function(){
+        $(".P5").fadeIn(400).delay(600).removeClass("none");
+        $(".P5").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h5").mouseleave(function(){
+        $(".P5").fadeOut(400).delay(600).removeClass("block");
+        $(".P5").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h6").mouseenter(function(){
+        $(".P6").fadeIn(400).delay(600).removeClass("none");
+        $(".P6").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h6").mouseleave(function(){
+        $(".P6").fadeOut(400).delay(600).removeClass("block");
+        $(".P6").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h7").mouseenter(function(){
+        $(".P7").fadeIn(400).delay(600).removeClass("none");
+        $(".P7").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h7").mouseleave(function(){
+        $(".P7").fadeOut(400).delay(600).removeClass("block");
+        $(".P7").fadeOut(400).delay(600).addClass("none");
+    });
+});
+
+$(document).ready(function(){
+    $(".h8").mouseenter(function(){
+        $(".P8").fadeIn(400).delay(600).removeClass("none");
+        $(".P8").fadeIn(400).delay(600).addClass("block");
+    });
+});
+
+$(document).ready(function(){
+    $(".h8").mouseleave(function(){
+        $(".P8").fadeOut(400).delay(600).removeClass("block");
+        $(".P8").fadeOut(400).delay(600).addClass("none");
+    });
+});
